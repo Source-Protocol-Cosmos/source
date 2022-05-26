9aeff75ba63af0ee2de3dc5deacc5932491fa167
Alejo Lovallo <49210713+AlejoLovallo@users.noreply.github.com>
2022-05-26 12:43:27 -0300

    Hermes relayer (#2)
    * wip hermes relayer conf
    
    * feat(hermes): cosmos hub relayer setup
    
    * add examples folder
    
    * feat(relayer): add osmosis config working for relayer
    
    * feat(scripts): set initial structure
    
    * feat(scripts): finish create clients script
    
    * feat(scripts): finish create connections script
    
    * feat(relayer): end relayer with juno
    
    * feat(scripts): finish create channels scripts
    
    * feat(scripts): finish scripts and add documentation
    
    * feat(fix): wip pr fixes
    
    * feat(fix): pr fixes
    
    * feat(doc): update readme

---
+++ hermes/create_clients.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+set -e
+
+usage() { echo "Usage: $0 -c <chain> -n <network_name>" 1>&2; exit 1; }
+
+while getopts ":c:n:" o; do
+    case "${o}" in
+        c)
+            c=${OPTARG}
+             ((c == "juno" || c == "osmosis" || c == "cosmos" )) || usage
+            CHAIN_NAME=$c
+            ;;
+        n)
+          NETWORK_NAME=${OPTARG}
+          ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${CHAIN_NAME}" ] || [ -z "${NETWORK_NAME}" ]; then
+    usage
+fi
+
+echo "--------------- CREATING HERMES CLIENTS -------------------"
+
+echo "CREATING CLIENT DEST_CHAIN: SOURCE SOURCE_CHAIN:${CHAIN_NAME}"
+
+CLIENT_ONE_OUTPUT=$(hermes -c hermes/configs/${CHAIN_NAME}_config.toml tx raw create-client sourcechain-testnet $NETWORK_NAME) 
+
+echo "CREATING SECOND CLIENT DEST_CHAIN: ${CHAIN_NAME} SOURCE_CHAIN: SOURCE"
+
+CLIENT_TWO_OUTPUT=$(hermes -c hermes/configs/${CHAIN_NAME}_config.toml tx raw create-client $NETWORK_NAME sourcechain-testnet) 
+
+echo "--------------- CLIENTS CREATED SUCCESSFULLY -------------------"
+echo $CLIENT_ONE_OUTPUT
+echo $CLIENT_TWO_OUTPUT
